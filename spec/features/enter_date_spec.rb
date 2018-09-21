38816bf646f54118b56be643215d0008236a52fb
Antonio <antoniodaria@msn.com>
2018-09-21 10:34:13 +0100

    refactoring of feature tests

--- spec/features/enter_date_spec.rb
+++ spec/features/enter_date_spec.rb
@@ -1,31 +1,40 @@
+require 'date'
+today = Date.today
+yesterday = Date.today - 1
+tomorrow = Date.today + 1
 
-feature "adding name" do
-  scenario "checks that is the user's birthday" do
-    visit('/')
-    fill_in :name, with: 'antonio'
-    fill_in :day, with: '21'
-    select('September', :from => 'month')
-    click_button 'GO'
-    expect(page).to have_content 'Happy birthday antonio!!'
-  end
 
+feature "adding user's details'" do
+  context "when it is the user's birthday" do
+    scenario "checks that is the user's birthday" do
+      visit('/')
+      fill_in :name, with: 'antonio'
+      fill_in :day, with: today.strftime("%d")
+      select(today.strftime("%B"), :from => 'month')
+      click_button 'GO'
+      expect(page).to have_content 'Happy birthday antonio!!'
+    end
+ end
+
+context "when the birthday is in 364 days" do
   scenario "checks that the user's birthday is in 364 days" do
     visit('/')
     fill_in :name, with: 'antonio'
-    fill_in :day, with: '20'
-    select('September', :from => 'month')
+    fill_in :day, with: yesterday.strftime("%d")
+    select(yesterday.strftime("%B"), :from => 'month')
     click_button 'GO'
     expect(page).to have_content 'antonio your birthday is in 364 days!!'
   end
+end
 
+context "when  ther is 1 day left to the user's birthday" do
   scenario "checks that the user's birthday is in 1 day" do
     visit('/')
     fill_in :name, with: 'antonio'
-    fill_in :day, with: '22'
-    select('September', :from => 'month')
+    fill_in :day, with: tomorrow.strftime("%d")
+    select(tomorrow.strftime("%B"), :from => 'month')
     click_button 'GO'
     expect(page).to have_content 'antonio your birthday is in 1 days!!'
   end
-
-
+ end
 end

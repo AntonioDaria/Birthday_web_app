1921882b4d4a412b32deb873fdea79639bd35c47
Antonio <antoniodaria@msn.com>
2018-09-21 11:00:18 +0100

    Refactoring the feature tests adding web helpers

--- spec/features/enter_date_spec.rb
+++ spec/features/enter_date_spec.rb
@@ -1,39 +1,27 @@
 require 'date'
+
 today = Date.today
 yesterday = Date.today - 1
 tomorrow = Date.today + 1
 
-
 feature "adding user's details'" do
   context "when it is the user's birthday" do
     scenario "checks that is the user's birthday" do
-      visit('/')
-      fill_in :name, with: 'antonio'
-      fill_in :day, with: today.strftime("%d")
-      select(today.strftime("%B"), :from => 'month')
-      click_button 'GO'
+      sign_in(today)
       expect(page).to have_content 'Happy birthday antonio!!'
     end
  end
 
 context "when the birthday is in 364 days" do
   scenario "checks that the user's birthday is in 364 days" do
-    visit('/')
-    fill_in :name, with: 'antonio'
-    fill_in :day, with: yesterday.strftime("%d")
-    select(yesterday.strftime("%B"), :from => 'month')
-    click_button 'GO'
+    sign_in(yesterday)
     expect(page).to have_content 'antonio your birthday is in 364 days!!'
   end
 end
 
 context "when  ther is 1 day left to the user's birthday" do
   scenario "checks that the user's birthday is in 1 day" do
-    visit('/')
-    fill_in :name, with: 'antonio'
-    fill_in :day, with: tomorrow.strftime("%d")
-    select(tomorrow.strftime("%B"), :from => 'month')
-    click_button 'GO'
+    sign_in(tomorrow)
     expect(page).to have_content 'antonio your birthday is in 1 days!!'
   end
  end

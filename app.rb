5b085bd7008ee7ba1badfbb35d3c9a3bb4c8e1e2
Antonio <antoniodaria@msn.com>
2018-09-21 09:44:28 +0100

    App completed fully functioning

--- app.rb
+++ app.rb
@@ -1,11 +1,36 @@
 require 'sinatra/base'
+require 'date'
 
 class Birthday < Sinatra::Base
 
-enable :session
+enable :sessions
 
-  get '/home' do
+  get '/' do
     erb :index
   end
 
+  post '/happyb' do
+    session[:name] = (params[:name])
+    session[:day] = (params[:day])
+    session[:month] = (params[:month])
+    redirect '/happyb'
+  end
+
+  get '/happyb' do
+    @name = session[:name]
+    @day = session[:day].to_i
+    @month = session[:month].to_i
+    calculate(Date.new(2018,@month,@day))
+    erb :happyb
+  end
+
+  def calculate(end_date)
+    today = Date.today
+    end_date = end_date
+    difference = (end_date - today).to_i
+    if difference < 0
+    difference += 365
+    end
+    difference
+  end
 end
---
+++ spec/features/enter_date_spec.rb
@@ -0,0 +1,21 @@
+
+feature "adding name" do
+  scenario "checks that is the user's birthday" do
+    visit('/')
+    fill_in :name, with: 'antonio'
+    fill_in :day, with: '21'
+    select('September', :from => 'month')
+    click_button 'GO'
+    expect(page).to have_content 'Happy birthday antonio!!'
+  end
+
+  scenario "checks that the user's birthday is in 364 days" do
+    visit('/')
+    fill_in :name, with: 'antonio'
+    fill_in :day, with: '20'
+    select('September', :from => 'month')
+    click_button 'GO'
+    expect(page).to have_content 'antonio your birthday is in 364 days!!'
+  end
+
+end
